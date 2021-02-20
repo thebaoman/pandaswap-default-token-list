65e0941813f1619f507f43f59bfea4a78347aca8
bao <kobykilgour@gmail.com>
2021-02-20 17:32:28 -0500

    v1

---
+++ src/buildList.js
@@ -0,0 +1,26 @@
+const { version } = require("../package.json");
+const xdai = require("./tokens/xdai.json");
+
+module.exports = function buildList() {
+  const parsed = version.split(".");
+  return {
+    name: "Pandaswap Default",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
+    },
+    tags: {},
+    logoURI: "https://www.bao.finance/static/media/bao-banner.90e5acb6.png",
+    keywords: ["Pandaswap", "bsc"],
+    tokens: [...xdai]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      }),
+  };
+};
